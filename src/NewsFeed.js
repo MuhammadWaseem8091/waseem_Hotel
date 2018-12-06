b58526d38a5254ad88a1bc24147be8e58b032d0b
lootster <sheldonkhlim@gmail.com>
2018-12-06 08:58:42 +0800

    api link

--- src/NewsFeed.js
+++ src/NewsFeed.js
@@ -1,6 +1,6 @@
 export default async function newsFeed() {
   const response = await fetch(
-    "https://newsapi.org/v2/everything?q=bitcoin&from=2018-11-05&sortBy=publishedAt&apiKey=" +
+    "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=" +
       process.env.REACT_APP_API_KEY
   );
   const data = await response.json();
